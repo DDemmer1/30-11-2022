9f68e2e56ca0743e86a111eaf0c01a5f6711235b
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2022-11-30 17:50:20 +0100

    initial commit

---
+++ src/de/demmer/dennis/EMail.java
@@ -0,0 +1,19 @@
+package de.demmer.dennis;
+
+public class EMail {
+	
+	String text;
+	String sender;
+	String recipient;
+	
+	
+	public EMail(String sender, String recipient) {
+		this.sender = sender;
+		this.recipient = recipient;
+		this.text = "";
+	}
+
+	
+	
+
+}
